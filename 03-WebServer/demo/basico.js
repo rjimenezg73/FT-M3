caf7dccd8c7a8441ea5d52512f9cf84221f29c7c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-08-24 15:47:24 -0500

    HW 03 WebServer

--- 03-WebServer/demo/basico.js
+++ 03-WebServer/demo/basico.js
@@ -2,8 +2,24 @@ var http = require('http'); // importamos el módulo http para poder trabajar co
 
 http.createServer( function(req, res){ // Creamos una serie de events listener, que van a escuchar por requests que ocurren en este socket
 	//Para crear un response empezamos escribiendo el header
-	res.writeHead(200, { 'Content-Type':'text/plain' }) //Le ponemos el status code y algunos pair-values en el header
-	res.end('Hola, Mundo!\n');
+	console.log(req.url);
+	if(req.url === '/'){
+		res.writeHead(200, { 'Content-Type':'text/plain' }) //Le ponemos el status code y algunos pair-values en el header
+		res.end('Hola, Mundo!\n');
+	}
+	else if(req.url === '/home'){
+		res.writeHead(200, { 'Content-Type':'text/plain' }) //Le ponemos el status code y algunos pair-values en el header
+		res.end('HOME\n');
+	}
+	else if(req.url === '/api'){
+		res.writeHead(200, { 'Content-Type':'text/plain' }) //Le ponemos el status code y algunos pair-values en el header
+		res.end('Estamos trabajando con nuestra API\n');
+	}
+	else{
+		res.writeHead(404, { 'Content-Type':'text/plain' }) //Le ponemos el status code y algunos pair-values en el header
+		res.end('NO ES UNA RUTA VÁLIDA!!!\n');
+	}
+	
 
 
 }).listen(1337, '127.0.0.1'); //Por último tenemos que especificar en que puerto y en qué dirección va a estar escuchando nuestro servidor
